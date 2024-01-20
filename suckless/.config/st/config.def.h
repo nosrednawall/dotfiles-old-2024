5f68144a15226ab17a6a9e34d1efa3759b247f71
Anderson José de Souza Inácio <nosrednawall@gmail.com>
2024-01-19 22:58:38 -0300

    aumentado a font do st term para 18

--- suckless/.config/st/config.def.h
+++ suckless/.config/st/config.def.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "monospace:pixelsize=15:antialias=true:autohint=true";
-static char *font2[] = { "monospace:pixelsize=15:antialias=true:autohint=true" };
+static char *font = "monospace:pixelsize=18:antialias=true:autohint=true";
+static char *font2[] = { "monospace:pixelsize=18:antialias=true:autohint=true" };
 static int borderpx = 2;
 
 /*
