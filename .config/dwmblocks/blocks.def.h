8704bc46ace6ed61b7ca1743b85815f1f2152920
Anderson José de Souza Inácio <nosrednawall@gmail.com>
2024-01-28 20:36:31 -0300

    alterado o espacamento entre os blocos

--- .config/dwmblocks/blocks.def.h
+++ .config/dwmblocks/blocks.def.h
@@ -41,4 +41,4 @@ static const Block blocks[] = {
 // sets delimeter between status commands. NULL character ('\0') means no
 // delimeter.
 static char delim[] =" ";
-static unsigned int delimLen = 2;
+static unsigned int delimLen = 1;
