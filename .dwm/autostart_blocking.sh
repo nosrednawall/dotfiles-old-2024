b421aa1b0059a3fc80c573a1a054ab00c5d07579
Anderson José de Souza Inácio <nosrednawall@gmail.com>
2024-01-27 21:11:12 -0300

    alterado configuração do picom

--- .dwm/autostart_blocking.sh
+++ .dwm/autostart_blocking.sh
@@ -7,7 +7,8 @@ pgrep -x xautolock > /dev/null || xautolock -time 10 -locker slock &
 redshift -P -O 4500 &
 nitrogen --restore &
 #compton --backend glx --vsync drm --unredir-if-possible --paint-on-overlay -b
-picom --animations -b --conf ~/.config/picom.conf
+#picom --animations -b --conf ~/.config/picom.conf
+picom --animations -b
 pgrep -x dunst > /dev/null || dunst &
 pgrep -x nm-tray > /dev/null || nm-tray &
 copyq &
