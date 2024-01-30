2c5090f28375c77ca70f75be774a25e3737dda77
Anderson José de Souza Inácio <nosrednawall@gmail.com>
2024-01-30 07:27:24 -0300

    alterado o tamanho da fonte, de 15 para 12

--- .dwm/config.h
+++ .dwm/config.h
@@ -15,8 +15,8 @@ static const int systraypinningfailfirst   = 1;   /* 1: if pinning fails, displa
 static const int showsystray               = 1;        /* 0 means no systray */
 static const int showbar                   = 1;        /* 0 means no bar */
 static const int topbar                    = 1;        /* 0 means bottom bar */
-static const char *fonts[]                 = { "Comic code:size=15","Hack Nerd Font:size=15","monospace:size=15","Font Awesome 6 Brands:size=15"  };
-static const char dmenufont[]              = "Comic code:size=15";
+static const char *fonts[]                 = { "Comic code:size=12","Hack Nerd Font:size=12","monospace:size=12","Font Awesome 6 Brands:size=12"  };
+static const char dmenufont[]              = "Comic code:size=12";
 
 /*gruvbox*/
 static const char col_gray1[]	    = "#282828";
