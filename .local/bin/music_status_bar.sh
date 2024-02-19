61422306e1358ab6696ee7745d8be53343d6f878
Anderson José de Souza Inácio <nosrednawall@gmail.com>
2024-02-18 22:23:05 -0300

    adicionado script de musica na barra do slstatus

---
+++ .local/bin/music_status_bar.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+title=$(mpc -f %title% current)
+
+if [ "$title" == ""  ]; then
+    echo ""
+else
+    icon="󰝚"
+    artist=$(mpc -f %artist% current)
+    time=$(mpc -f %time% current)
+
+    echo "$icon $title [$artist] [$time]"
+fi
