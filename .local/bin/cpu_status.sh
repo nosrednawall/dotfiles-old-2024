f3ab5307d37fe43f6fcfffb77c474c7dfcbfd8be
Anderson José de Souza Inácio <nosrednawall@gmail.com>
2024-02-20 17:30:59 -0300

    adicionado

---
+++ .local/bin/cpu_status.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+tempCpu=$(sensors | awk '/edge/ {print$2}' | sed 's/+//')
+#usageCpu=$(top -bn1 | grep load | awk '{printf "%.2f%%\t\t\n", $(NF-2)}')
+icon=""
+
+read cpu a b c previdle rest < /proc/stat
+prevtotal=$((a+b+c+previdle))
+sleep 0.5
+read cpu a b c idle rest < /proc/stat
+total=$((a+b+c+idle))
+usageCpu=$((100*( (total-prevtotal) - (idle-previdle) ) / (total-prevtotal) ))
+
+echo "$icon $usageCpu% $tempCpu"
