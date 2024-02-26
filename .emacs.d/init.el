7c5402dc1b1558ca9dd24098291bd42aadb94528
Anderson José de Souza Inácio <nosrednawall@gmail.com>
2024-02-25 21:25:36 -0300

    Renomeado o arquivo myinit.org para emacs.org

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -48,17 +48,24 @@
 
 
 ;; Carrega os pacotes que estão no arquivo ORG - Não uso mais
-(org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
-;; Coisas Use Package
+(org-babel-load-file (expand-file-name "~/.emacs.d/emacs.org"))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(electric-indent-mode t)
- '(package-selected-packages
-   '(markdown-preview-mode markdown-mode indent-guide rainbow-mode ggtags auto-complete web-mode elpy org-bullets counsel neotree spaceline all-the-icons centaur-tabs buffer-flip which-key gruvbox-theme try projectile dashboard use-package seq magit ess))
- '(warning-suppress-types '((comp) (comp) (comp) (comp))))
+ '(warning-suppress-types
+   '((comp)
+     (comp)
+     (comp)
+     (comp)
+     (comp)
+     (comp)
+     (comp)
+     (comp)
+     (comp)
+     (comp)
+     (comp))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
