6eb5a7f98ffaa98444c3bbfcd31efe86cd28727b
Anderson José de Souza Inácio <anderson.inacio.dev@gmail.com>
2024-05-25 13:23:44 -0300

    adicionado patch scractpads

--- .config/suckless/dwm_git/dwm_solarized/config.h
+++ .config/suckless/dwm_git/dwm_solarized/config.h
@@ -40,6 +40,21 @@ static const char *colors[][3]      = {
 	[SchemeInfoNorm]   = { col_2, col_1, col_1 }, // infobar middle  unselected {text,background,not used but cannot be empty}
 };
 
+typedef struct {
+	const char *name;
+	const void *cmd;
+} Sp;
+const char *spcmd1[] = {"st", "-n", "spterm", "-g", "120x34", NULL };
+const char *spcmd2[] = {"st", "-n", "spfm", "-g", "144x41", "-e", "ranger", NULL };
+const char *spcmd3[] = {"bitwarden", NULL };
+static Sp scratchpads[] = {
+	/* name          cmd  */
+	{"spterm",      spcmd1},
+	{"spranger",    spcmd2},
+	{"bitwarden",   spcmd3},
+};
+
+
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5" };
 
@@ -57,6 +72,9 @@ static const Rule rules[] = {
 	{ "Gimp",						NULL,		NULL,		0,				1,				-1 },
 	{ "copyq",						NULL,		NULL,		0,				1,				-1 },
 	{ "mpv",						NULL,		NULL,		0,				1,				-1 },
+	{ NULL,		  "spterm",		NULL,		SPTAG(0),		1,			 -1 },
+	{ NULL,		  "spfm",		NULL,		SPTAG(1),		1,			 -1 },
+	{ NULL,		  "keepassxc",	NULL,		SPTAG(2),		0,			 -1 },
 };
 
 /* layout(s) */
@@ -111,8 +129,10 @@ static const Key keys[] = {
 	/* modifier                     key        		function        argument */
 	{ MODKEY,                       XK_p,      		spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, 		spawn,          {.v = termcmd } },
-	//{ MODKEY,                       XK_s,      		togglescratch,  {.v = scratchpadcmd } },
 	{ MODKEY,                       XK_b,      		togglebar,      {0} },
+	{ MODKEY,            			XK_s,  	   togglescratch,  {.ui = 0 } },
+	{ MODKEY,            			XK_r,	   togglescratch,  {.ui = 1 } },
+	{ MODKEY,            			XK_n,	   togglescratch,  {.ui = 2 } },
 
 	/*Modimentacao das janelas*/
 	{ MODKEY,                       XK_Right,      	focusstack,     {.i = +1 } },
@@ -247,7 +267,7 @@ static const Button buttons[] = {
 	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
 	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
 	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
-	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
+	{ ClkClientWin,         MODKEY,         Button1,        resizemouse,    {0} },
 	{ ClkTagBar,            0,              Button1,        view,           {0} },
 	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
