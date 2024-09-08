f35bc6205d427af2799268d115a3bf505fadc316
anderson <anderson.inacio.dev@gmail.com>
2024-09-08 10:16:59 -0300

    corrigido alguns trechos

--- .config/debian/ide_programs.sh
+++ .config/debian/ide_programs.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
 
 # IDE
-sudo apt install emacs-gtk geany
+sudo apt install -y emacs-gtk geany
 git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.config/emacs
-~/.config/emacs/bin/doom install  # Cuidado para nao sobreescrever os arquivos do dotfiles
+
+escolha_instalar_doom() {
+    local wm_name="$1"
+    echo "$wm_name Instalação"
+    echo "1. Instalar doom agora"
+    echo "Ou ENTER para pular"
+    read -r choice
+
+   case "$choice" in
+        1)
+            ~/.config/emacs/bin/doom install  # Cuidado para nao sobreescrever os arquivos do dotfiles
+            ;;
+        *)
+            echo "Pulando essa etapa, e instalando o resto..."
+            ;;
+    esac
+
+    # Adding a couple of line returns
+    echo -e "\n\n"
+}
+
+escolha_instalar_doom
--- .config/debian/misc_programs.sh
+++ .config/debian/misc_programs.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 # Pass, qtpass, pinentry e askpass
-sudo apt install pass qtpass pinentry-gnome3 ssh-askpass-gnome
+sudo apt install -y pass qtpass pinentry-gnome3 ssh-askpass-gnome
 
 # O resto dos programas
-sudo apt install feh rofi network-manager xautolock picom dunst xdotool copyq solaar blueman flameshot btop qalculate-gtk libnotify-bin gpicview fonts-bebas-neue yt-dlp gpm aria2
+sudo apt install -y feh rofi network-manager xautolock picom dunst xdotool copyq solaar blueman flameshot btop qalculate-gtk libnotify-bin gpicview fonts-bebas-neue yt-dlp gpm aria2
 
 # Fontes microsoft
-sudo apt install cabextract curl fontconfig xfonts-utils
-sudo apt install ttf-mscorefonts-installer
+sudo apt install -y cabextract curl fontconfig xfonts-utils
+sudo apt install -y ttf-mscorefonts-installer
 
 # Office
-sudo apt install libreoffice-gtk3 libreoffice-l10n-pt-br libreoffice atril xournalpp
+sudo apt install -y libreoffice-gtk3 libreoffice-l10n-pt-br libreoffice atril xournalpp
 
 # Google-chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt install ./google-chrome-stable_current_amd64.deb
+sudo apt install -y ./google-chrome-stable_current_amd64.deb
 rm -rf google-chrome-stable_current_amd64.deb
